63f532de59dc94fc5dcd50a94d5177357ffe4bcc
gnvidal <45744107+gnvidal@users.noreply.github.com>
2021-07-05 18:50:32 -0400

    Update Rabbits_and_Recurrence_Relations.py

--- Solutions/Rabbits_and_Recurrence_Relations.py
+++ Solutions/Rabbits_and_Recurrence_Relations.py
@@ -1,9 +1,12 @@
 memo={}
+#now, this is recursive programming, it's soo low! 
 def fib_rec(n, k):
 	if n==0 or n==1:
 		return 1
 	return fib_rec(n-1) + fib_rec(n-2)
+#so, is better to use dunamic programming
 #now, we've used dynamic programming
+#in this code to store results in dictionary, is call memo. this variable is 1 line code
 def fib_dina(n, k=1):
     args=(n,k)
     if args in memo:
